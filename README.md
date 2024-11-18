2e13596690fbf6193c4a82917a9664e50fe98966
Troy Chaplin <troy.chaplin@gmail.com>
2024-11-17 21:36:43 -0500

    add: tailwind theme config

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind Theme Config
+
+This is a Tailwind CSS plugin to add the theme globals to [IDC UI Kit](https://github.com/troychaplin/idc-ui-kit)
---
+++ index.js
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "idc-black": {
+          DEFAULT: '#191919',
+          50: '#f6f6f6',
+          100: '#e7e7e7',
+          200: '#d1d1d1',
+          300: '#b0b0b0',
+          400: '#888888',
+          500: '#6d6d6d',
+          600: '#5d5d5d',
+          700: '#4f4f4f',
+          800: '#454545',
+          900: '#3d3d3d',
+          950: '#191919',
+        },
+        "idc-blue": {
+          DEFAULT: "#3678b9",
+          50: "#f3f6fc",
+          100: "#e6eef8",
+          200: "#c7daf0",
+          300: "#95bbe4",
+          400: "#5d99d3",
+          500: "#3678b9",
+          600: "#2862a1",
+          700: "#214e83",
+          800: "#1f446d",
+          900: "#1f3a5b",
+          950: "#14253d",
+        },
+        "idc-orange": {
+          DEFAULT: "#ffac33",
+          50: "#fff8eb",
+          100: "#ffedc6",
+          200: "#ffd888",
+          300: "#ffbe4a",
+          400: "#ffac33",
+          500: "#f98007",
+          600: "#dd5b02",
+          700: "#b73c06",
+          800: "#942e0c",
+          900: "#7a260d",
+          950: "#461102",
+        },
+      },
+      margin: {
+        "offset-center": "calc(-50vw + 50%)",
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+};
