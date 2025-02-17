0a0841d5dda6716bc1f417aaafc09b8265a6f683
troychaplin <troy.chaplin@gmail.com>
2025-02-17 16:46:34 -0500

    add: 4xl breakpoint

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ module.exports = {
       fontFamily: {
         open: ['Open Sans', 'sans-serif'],
       },
+      screens: {
+        '4xl': '896px',
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: [
